70e62eb71a7c80a1cf78e79c0bbcfe1f2942a201
nicigomez <nicolas_lerch@gmx.de>
2022-05-18 14:19:47 +0200

    changed headline :)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h>Hello from Amplify</h>
+        <h>Hello from Amplify :)</h>
         <a
           className="App-link"
           href="https://reactjs.org"
