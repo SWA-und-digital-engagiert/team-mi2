124bc31da725a5aa3bd692df04256406af7cae3a
nicigomez <nicolas_lerch@gmx.de>
2022-05-24 13:37:21 +0200

    push auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { withAuthenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+Amplify.configure(awsconfig);
+
+export default withAuthenticator(App);
+
+function App({ signOut, user }) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h>Hello from Amplify :)</h>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <button onClick={signOut}>Sign out</button>
+          <h2>Hello my Users</h2>
       </header>
     </div>
   );
 }
-
-export default App;
