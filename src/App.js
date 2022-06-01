7c458107c06886e0a8a79251dfcccee847cc328e
nicigomez <nicolas_lerch@gmx.de>
2022-06-01 13:04:10 +0200

    reset app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import Amplify from "aws-amplify";
-import awsconfig from "./aws-exports";
-import { withAuthenticator } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css';
 
-Amplify.configure(awsconfig);
-
-export default withAuthenticator(App);
-
-
-function App({ signOut}) {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <button onClick={signOut}>Sign out</button>
-          <h2>Hello Users!</h2>
-      </header>
-    </div>
-  );
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
+
+export default App;
