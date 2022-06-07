9065d2a2b7f527653a158c8ff751ba831a561405
Christoph Schwarz <christoph.schwarz.2002@gmail.com>
2022-06-07 19:23:05 +0200

    Added Create Account Features

--- src/App.js
+++ src/App.js
@@ -1,45 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Amplify} from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+
+import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-function App({ isPassedToWithAuthenticator, signOut, user }) {
-    if (!isPassedToWithAuthenticator) {
-        throw new Error(`isPassedToWithAuthenticator was not provided`);
-    }
-  return (
-    <div className="App">
-      <header className="App-header">
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-          <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          north virginia
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const formFields = {
+    signIn: {
+        username: {
+            labelHidden: false,
+            placeholder: 'Enter Your Email Here',
+            isRequired: true,
+            label: 'Email:'
+        },
+    },
+    signUp: {
+        username: {
+            labelHidden: false,
+            placeholder: 'Your Real Full Name',
+            isRequired: true,
+            label: 'Name:'
+        },
+        password: {
+            labelHidden: false,
+            placeholder: 'Choos a safe Password',
+            isRequired: true,
+            label: 'Password:'
+        },
+        nickname: {
+            labelHidden: false,
+            placeholder: 'How your Friends call you',
+            isRequired: true,
+            label: 'Nickname (Displayed in Chat usw.):'
+        },
+        email: {
+            labelHidden: false,
+            placeholder: 'Email',
+            isRequired: true,
+            label: 'Email:'
+        },
+        phone_number: {
+            labelHidden: false,
+            placeholder: 'Phone Number (+49) (0) 123 123 45 678',
+            isRequired: true,
+            label: 'Phone Number:',
+            dialCodeList: ['+49']
+        },
+        birthdate: {
+            labelHidden: false,
+            placeholder: 'tt.mm.jjjj',
+            isRequired: true,
+            label: 'Birthdate:',
+        },
+        enter_code: {
+            labelHidden: false,
+            placeholder: 'The Entercode to enter this organisation',
+            isRequired: true,
+            label: 'Entercode:',
+        }
+    },
 }
 
-export default withAuthenticator(App);
-
-export async function getStaticProps() {
-    return {
-        props: {
-            isPassedToWithAuthenticator: true,
-        },
-    };
+export default function App() {
+    return (
+        <Authenticator
+        formFields={formFields}
+        signUpAttributes={['email', 'phone_number', 'nickname', 'birthdate',]}
+        >
+            {({ signOut, user }) => (
+                <main>
+                    <h1>Hello {user.username}</h1>
+                    <button onClick={signOut}>Sign out</button>
+                </main>
+            )}
+        </Authenticator>
+    );
 }
